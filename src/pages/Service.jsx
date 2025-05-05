84a127531afb253630d49062d8df4828d0aa5cf3
Akash <ak.parmar1442000@gmail.com>
2025-05-05 11:21:21 +0530

    service page ui done and functionality done

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -30,14 +30,14 @@ const Service = () => {
       }
   return (
     <section className='home-page'>
-      <section className="h-dvh w-full bg-black ff-allen grid place-content-center relative overflow-hidden relative">
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 text-white text-2xl xs:text-[32px] sm:text-4xl  md:text-6xl xl:text-7xl flex flex-col items-center justify-center gap-4 md:gap-8 z-[1]  backdrop-blur-lg px-9 pt-8 pb-6">
-          <p className=''>
+      <section className="h-dvh w-full bg-black ff-allen grid place-content-center overflow-hidden relative">
+        <img src={service?.imgUrl} alt={service?.name} className='h-full w-full absolute object-cover' />ß
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 text-white text-2xl xs:text-[32px] sm:text-4xl  md:text-6xl xl:text-7xl flex flex-col items-center justify-center gap-4 md:gap-8 z-[1]  backdrop-blur-md px-9 pt-8 pb-6 rounded-t-3xl">
+          <p className='whitespace-nowrap'>
             {service?.name}
           </p>
         </div>
-        <img src={service?.imgUrl} alt={service?.name} className='h-full w-full absolute object-cover' />
-        <div onClick={handleScroll} className='cursor-pointer select-none h-14 w-14 rounded-full bg-black absolute bottom-6 right-6 text-xs flex flex-col items-center justify-center text-white'>
+        <div onClick={handleScroll} className='hidden sm:flex cursor-pointer select-none h-14 w-14 rounded-full bg-black absolute bottom-6 right-6 text-xs  flex-col items-center justify-center text-white '>
               <p className='mt-1'>
                   Scroll
               </p>
@@ -46,8 +46,13 @@ const Service = () => {
               </p>
         </div>
       </section>
-      <section ref={serviceRef} className='max-w-1180 mx-auto w-full  gap-8 pt-20 px-4 text-xl font-light  text-center text-black/80'>
+      <section ref={serviceRef} className='max-w-1180 mx-auto w-full gap-8 pt-20 px-4 text-xl font-light  text-center text-black/80'>
       {service?.content}
+
+      <a href={`https://wa.me/9899125551?text=${encodeURIComponent(`I'm interested in your service: ${service.name}`)}`} className='block border border-primary rounded-lg text-primary ff-allen text-lg w-max px-5 py-3 mt-8 mx-auto'>
+                Let's Connect
+              </a>
+
       </section>
       <WhoWeServed/>
 
